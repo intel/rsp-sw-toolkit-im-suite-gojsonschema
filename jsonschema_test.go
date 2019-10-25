a011ce9205ba6e7802ff3ba025e76e245ce45ba8
alexander-j-saites <alexander.j.saites@intel.com>
2019-10-25 13:04:24 -0700

    Change test server port to avoid Jenkins conflict It'd be better if it were able to use a dynamic port...

--- jsonschema_test.go
+++ jsonschema_test.go
@@ -129,7 +129,7 @@ func TestSuite(t *testing.T) {
 	wd = filepath.Join(wd, "testdata")
 
 	go func() {
-		err := http.ListenAndServe(":1234", http.FileServer(http.Dir(filepath.Join(wd, "remotes"))))
+		err := http.ListenAndServe(":4321", http.FileServer(http.Dir(filepath.Join(wd, "remotes"))))
 		if err != nil && err != http.ErrServerClosed {
 			t.Fatalf("Error starting server: %s\n", err.Error())
 		}
